a8db53a04226565e2ec4e0e62b1d370f8f2fdf9c
Richard Zhan <zhan97@gmail.com>
2014-10-27 21:51:54 -0700

    kms

--- repeatingxor/repeatingxor.py
+++ repeatingxor/repeatingxor.py
@@ -8,7 +8,7 @@ while (i+2<len(f)):
 
 offset=12 #LENGTH IS 12
 prekappa=0
-while (offset < 13):
+while (offset < len(stuff)):
     
     shift = [stuff[x+offset] for x in xrange(len(stuff)-offset)]
     shift+=stuff[0:offset]
@@ -16,20 +16,21 @@ while (offset < 13):
     text=""
     passw=""
     decrypt=""
-    while (i < 30):
-        text=text+stuff[i]
-        passw=passw+shift[i]
-        if len((hex(int(stuff[i],16) ^ int(shift[i],16)))[2:]) == 1:
-            decrypt+="0"
-        elif len((hex(int(stuff[i],16) ^ int(shift[i],16)))[2:]) == 0:
-            decrypt+="00"
-        decrypt=decrypt+(hex(int(stuff[i],16) ^ int(shift[i],16)))[2:]
+    while (i < 1):
+        #if (int(stuff[i],16) ^ int(shift[i],16) == 0):
+        #    prekappa+=1
+        #text=text+stuff[i]
+        #passw=passw+shift[i]
+        #if int(stuff[i],16) ^ int(shift[i],16) <= 30:
+        decrypt=decrypt+str((int(stuff[i],16) ^ int(shift[i],16)))+"."
         i+=1
+    decrypt+="\n"
+    #print(str(offset) + "\t"+str(prekappa/(len(stuff)-1.0)))
     offset+=12
     prekappa=0
     
-    print "text:",text
-    print "passw:",passw
+    #print "text:",text
+    #print "passw:",passw
     print "decrypt:",decrypt
-    print "\n"
+    #print "\n"
     
